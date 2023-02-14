5dd9c2da58c917b5e2269803f289f2e93325752d
Lukas Maynard <lmaynard8@ivytech.edu>
2023-02-14 11:29:40 -0500

    classes update

--- src/Activity12/classes.java
+++ src/Activity12/classes.java
@@ -4,21 +4,27 @@ public class classes {
 
     public static class Planet {
         String name;
+        String size;
+        double distanceFromSun;
 
-        public Planet(String name) {
+        public Planet(String name, String size, double distanceFromSun) {
             this.name = name;
-
+            this.size = size;
+            this.distanceFromSun = distanceFromSun;
         }
     }
 
     public static class Satellite{
         String name;
+        String homePlanet;
+        double speed;
 
-        public Satellite(String name){
+        public Satellite(String name, String homePlanet, double speed){
             this.name = name;
+            this.homePlanet = homePlanet;
+            this.speed = speed;
         }
     }
-
 
     public static void main(String[] args) {
 
