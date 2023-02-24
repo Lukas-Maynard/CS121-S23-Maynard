37e75cf00e529c7df2727bdd023252eba9154ee6
Lukas Maynard <104531583+Lukas-Maynard@users.noreply.github.com>
2023-02-24 15:54:01 -0500

    Array Demo assignment

--- src/Activity16/ArrayDemo3.java
+++ src/Activity16/ArrayDemo3.java
@@ -1,4 +1,17 @@
 package src.Activity16;
 
 public class ArrayDemo3 {
+    public static void main(String[] args) {
+        // 3D array
+        int[][][] array = {{{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}}};
+
+        for(int[][] i:array){
+            for(int[] j:i){
+                for(int k:j){
+                    System.out.println(k);
+                }
+            }
+        }
+    }
 }
+
