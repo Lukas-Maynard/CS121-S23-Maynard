023b49a0c1425d997ef4a8ba9f8882cf74185e39
Lukas Maynard <104531583+Lukas-Maynard@users.noreply.github.com>
2023-03-22 13:33:34 -0400

    upload selection sort

---
+++ src/Activity26_selection_injection_sort/Main.java
@@ -0,0 +1,14 @@
+package src.Activity26_selection_injection_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        SelectionSort sort = new SelectionSort();
+        int[] unsortedArray = sort.getArray();
+        System.out.println("Unsorted array:"+ Arrays.toString(unsortedArray));
+
+        int[] sortedArray = sort.sortArray(unsortedArray);
+        System.out.println("Sorted array:"+Arrays.toString(sortedArray));
+    }
+}
---
+++ src/Activity26_selection_injection_sort/SelectionSort.java
@@ -0,0 +1,30 @@
+package src.Activity26_selection_injection_sort;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    public int[] getArray(){
+        int[] array = new int[5];
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter 5 unsorted integers");
+        for (int i=0;i<5;i++){
+            String input = console.next();
+            array[i] = Integer.parseInt(input);
+        }
+        return array;
+    }
+    public int[] sortArray(int[] array){
+        for (int i = 0;i<array.length-1 ;i++){
+            int min = i;
+            for (int j=i+1;j<array.length;j++){
+                if (array[j]>array[min]){
+                    min = array[j];
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+        return array;
+    }
+}
