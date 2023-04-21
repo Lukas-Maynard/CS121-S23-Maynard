84b6632867e6ce08ed2464402764677f946980b5
Lukas Maynard <104531583+Lukas-Maynard@users.noreply.github.com>
2023-04-20 21:26:08 -0400

    hashmap assignments

---
+++ src/Activity30_Hashmaps/Hashmaps.java
@@ -0,0 +1,4 @@
+package src.Activity30_Hashmaps;
+
+public class Hashmaps {
+}
---
+++ src/Activity30_Hashmaps/Main.java
@@ -0,0 +1,7 @@
+package src.Activity30_Hashmaps;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
