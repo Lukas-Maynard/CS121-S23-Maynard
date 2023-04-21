4bb84f005992b4559115b603a5fe92d28022f237
Lukas Maynard <lmaynard8@ivytech.edu>
2023-04-20 22:21:18 -0400

    hashmap assignment

--- src/Activity30_Hashmaps/Main.java
+++ src/Activity30_Hashmaps/Main.java
@@ -2,6 +2,16 @@ package src.Activity30_Hashmaps;
 
 public class Main {
     public static void main(String[] args) {
+        Hashmaps planetHM = new Hashmaps();
 
+        planetHM.add_Planet("Earth",3958.8);
+        planetHM.add_Planet("Mars", 2106.1);
+        planetHM.add_Planet("Jupiter", 3760.4);
+
+        planetHM.display_All();
+
+        planetHM.remove_Planet("Earth");
+
+        planetHM.display_All();
     }
 }
