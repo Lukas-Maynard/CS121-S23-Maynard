9cebcc5223602f8e02e2d5698e116c3c3d4c7a92
Lukas Maynard <lmaynard8@ivytech.edu>
2023-04-23 00:00:28 -0400

    Stacks "brackets" assignment

---
+++ src/Activity32_stackedBrackets/BalancedBrackets.java
@@ -0,0 +1,32 @@
+package src.Activity32_stackedBrackets;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class BalancedBrackets {
+
+    public String checkBalance(String input) {
+        //char[] bracketsArray = {'(','[','{','}',']',')'};
+        Stack<Character> stack = new Stack<>();
+        char previous;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '(' || c =='[' || c =='{') {
+                stack.push(c);
+            } else {
+                try {
+                    stack.pop();
+                } catch (EmptyStackException e){
+                    return "unbalanced";
+                }
+            }
+        }
+
+        if (stack.empty()) {
+            return "balanced";
+        } else {
+            return "unbalanced";
+        }
+    }
+}
---
+++ src/Activity32_stackedBrackets/BalancedBracketsTester.java
@@ -0,0 +1,20 @@
+package src.Activity32_stackedBrackets;
+
+public class BalancedBracketsTester {
+    public static void main(String[] args) {
+        BalancedBrackets balanced = new BalancedBrackets();
+
+        System.out.println(balanced.checkBalance("()"));
+        System.out.println(balanced.checkBalance("(()())"));
+        System.out.println(balanced.checkBalance("((((()(())))))"));
+        System.out.println(balanced.checkBalance("(()"));
+        System.out.println();
+
+        System.out.println(balanced.checkBalance("())"));
+        System.out.println(balanced.checkBalance(")()"));
+        System.out.println();
+
+        System.out.println(balanced.checkBalance("([{}])"));
+        System.out.println(balanced.checkBalance("([{])"));
+    }
+}
